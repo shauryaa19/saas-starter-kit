78329447b836b696eb58832333b13b230c54ce3a
shauryaa19 <120932081+shauryaa19@users.noreply.github.com>
2024-11-02 03:39:01 +0530

    prisma setup

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// used to connect the database, can be taken from prisma examples github
+import { PrismaClient } from "@prisma/client"
+
+const prismaClientSinglton = () => {
+    return new PrismaClient()
+}
+
+type prismaClientSinglton = ReturnType<typeof prismaClientSinglton>
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+
+const prisma = globalForPrisma.prisma ?? prismaClientSinglton()
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+export default prisma
